c2782c5d66a16a0dce3f049b9f2e8caf14f39497
dbedrossian <danny.bedrossian@gmail.com>
2023-01-28 17:34:22 -0500

    finished header

---
+++ src/components/Navbar.js
@@ -0,0 +1,32 @@
+import React from "react";
+import '../styles/Navbar.css';
+
+const styles = {
+    navbarNameStyle: {
+        justifyContent: 'left',
+    },
+    navbarStyle: {
+        justifyContent: 'right',
+        display: 'flex',
+        color: 'white',
+        padding: '15px'
+    }
+}
+
+function Navbar() {
+    return (
+        <nav className="navbar">
+            <div style={styles.navbarNameStyle}>
+            <a href="/">Danny Bedrossian</a>
+            </div>
+            <div style={styles.navbarStyle}>
+                <h5 className="headerText">About</h5>
+                <h5 className="headerText">Portfolio</h5>
+                <h5 className="headerText">Contact</h5>
+                <h5 className="headerText">Resume</h5>
+            </div>
+        </nav>
+    )
+};
+
+export default Navbar;
